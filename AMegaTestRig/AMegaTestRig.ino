0c725643aa3f25ef98551362e98c609048eb0f58
R H Shorts <rhshorts@gmail.com>
2017-06-10 18:27:26 +0100

    Test Rig
    Board connected to an Arduino Mega

---
+++ AMegaTestRig/AMegaTestRig.ino
@@ -0,0 +1,12 @@
+// Arduino Mega Test Rig
+// Created Saturday 10th June 2017
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
